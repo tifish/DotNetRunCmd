47b15858fcbcf2681ea6b00568c593628b1c9513
Tinyfish <tifish@gmail.com>
2026-01-25 16:16:53 +0800

    Add Reg and RegValue. Update version to 1.1.0

---
+++ DotNetRunCmd/Windows/RegValue.cs
@@ -0,0 +1,103 @@
+using System.Runtime.Versioning;
+using Microsoft.Win32;
+
+namespace DotNetRun;
+
+[SupportedOSPlatform("windows")]
+public struct RegistryValue(string keyName, string? valueName)
+{
+    public string KeyName { get; set; } = keyName;
+    public string? ValueName { get; set; } = string.IsNullOrEmpty(valueName) ? null : valueName;
+
+    public readonly string? GetValue(string? defaultValue)
+    {
+        return Reg.GetValue(KeyName, ValueName, defaultValue);
+    }
+
+    public readonly int GetValue(int defaultValue)
+    {
+        return Reg.GetValue(KeyName, ValueName, defaultValue);
+    }
+
+    public readonly byte[]? GetBinaryValue(byte[]? defaultValue)
+    {
+        return Reg.GetBinaryValue(KeyName, ValueName, defaultValue);
+    }
+
+    public readonly bool GetBitFromBinary(int bitIndex, bool defaultValue)
+    {
+        var binaryValue = GetBinaryValue(null);
+        if (binaryValue == null)
+            return defaultValue;
+
+        var byteIndex = bitIndex / 8;
+        if (byteIndex >= binaryValue.Length)
+            return defaultValue;
+
+        return (binaryValue[byteIndex] & (1 << (bitIndex % 8))) != 0;
+    }
+
+    public readonly void SetBitToBinary(int bitIndex, bool value)
+    {
+        var binaryValue = GetBinaryValue(null);
+        if (binaryValue == null)
+            binaryValue = new byte[1];
+
+        var byteIndex = bitIndex / 8;
+        if (byteIndex >= binaryValue.Length)
+        {
+            var newBinaryValue = new byte[byteIndex + 1];
+            binaryValue.CopyTo(newBinaryValue, 0);
+            binaryValue = newBinaryValue;
+        }
+
+        if (value)
+            binaryValue[byteIndex] |= (byte)(1 << (bitIndex % 8));
+        else
+            binaryValue[byteIndex] &= (byte)~(1 << (bitIndex % 8));
+
+        SetBinaryValue(binaryValue);
+    }
+
+    public readonly void SetValue(string value)
+    {
+        Reg.SetValue(KeyName, ValueName, value);
+    }
+
+    public readonly void SetValue(int value)
+    {
+        Reg.SetValue(KeyName, ValueName, value);
+    }
+
+    public readonly void SetBinaryValue(byte[] value)
+    {
+        Reg.SetBinaryValue(KeyName, ValueName, value);
+    }
+
+    public readonly void SetValue(object value, RegistryValueKind valueKind)
+    {
+        Reg.SetValue(KeyName, ValueName, value, valueKind);
+    }
+
+    public readonly void DeleteValue()
+    {
+        Reg.DeleteValue(KeyName, ValueName!);
+    }
+
+    public readonly void DeleteKey()
+    {
+        Reg.DeleteKey(KeyName);
+    }
+
+    public bool HasKey()
+    {
+        using var key = Reg.OpenKey(KeyName);
+        return key != null;
+    }
+
+    public bool HasValue()
+    {
+        using var key = Reg.OpenKey(KeyName);
+        return key?.GetValue(ValueName) != null;
+    }
+}
